6ac1b61e4cf73127fd0268a343f150e316c554b4
Novav20 <illus.kun@gmail.com>
2026-01-09 08:12:35 -0500

    feat(server): implement asset routes with GET endpoint

---
+++ tmg-server/src/routes/asset.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { AssetController } from "../controllers/asset.controller";
+import { AssetService } from "../services/asset.service";
+import { PrismaService } from "../services/prisma.service";
+
+const prisma = PrismaService.getInstance().client;
+const assetService = new AssetService(prisma);
+const assetController = new AssetController(assetService);
+
+const router = Router();
+
+router.get("/", assetController.getAll);
+
+export default router;
