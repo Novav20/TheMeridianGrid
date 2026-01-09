0e34c1b16dcd9d73bd0a4c22683cd036aba0fe9e
Novav20 <illus.kun@gmail.com>
2026-01-09 07:11:42 -0500

    feat(server): implement asset creation logic in service

--- tmg-server/src/services/asset.service.ts
+++ tmg-server/src/services/asset.service.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { CreateAssetDto } from "../schemas/asset.schema";
 
 /**
  * Service: AssetService
@@ -7,7 +8,21 @@ import { PrismaClient } from "@prisma/client";
  */
 export class AssetService {
   constructor(private prisma: PrismaClient) {}
+
+  /**
+   * Retrieves all assets from the database.
+   */
   async getAllAssets() {
     return this.prisma.asset.findMany();
+  }
+
+  /**
+   * Creates a new asset.
+   * @param data The asset data to create.
+   */
+  async createAsset(data: CreateAssetDto) {
+    return this.prisma.asset.create({
+      data,
+    });
   }
 }
